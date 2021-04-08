a03e03a02cda06460d343d9f00eb8d8b283a7a88
Gustavo Borjas <gustavodanielborjas@outlook.com>
2021-04-08 13:22:03 -0400

    End of Section 9

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import 'semantic-ui-css/semantic.min.css';
 import App from './app/layout/App';
+import 'react-calendar/dist/Calendar.css'
 import './app/layout/styles.css';
 import { store, StoreContext } from './app/stores/store';
 import reportWebVitals from './reportWebVitals';
