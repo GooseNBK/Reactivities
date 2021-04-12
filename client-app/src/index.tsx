4cdbdc0f8c2d9d6ad972581a3f73b4f6af50d832
Gustavo Borjas <gustavodanielborjas@outlook.com>
2021-04-12 14:58:34 -0400

    End of section 11

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -5,6 +5,7 @@ import 'semantic-ui-css/semantic.min.css';
 import App from './app/layout/App';
 import 'react-calendar/dist/Calendar.css'
 import 'react-toastify/dist/ReactToastify.min.css';
+import 'react-datepicker/dist/react-datepicker.css';
 import './app/layout/styles.css';
 import { store, StoreContext } from './app/stores/store';
 import reportWebVitals from './reportWebVitals';
